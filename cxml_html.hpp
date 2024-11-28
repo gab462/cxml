d0964c1ee6dabe3ead5f983a86f430fd12189bec
gab462 <62474485+gab462@users.noreply.github.com>
2024-11-28 10:42:11 -0300

    Implement more tags and more complete example

--- cxml_html.hpp
+++ cxml_html.hpp
@@ -31,6 +31,11 @@ namespace cxml {
         header(Args&&... args): tree{"header", std::forward<Args>(args)...} {}
     };
 
+    struct hgroup: tree {
+        template<typename... Args>
+        hgroup(Args&&... args): tree{"hgroup", std::forward<Args>(args)...} {}
+    };
+
     struct footer: tree {
         template<typename... Args>
         footer(Args&&... args): tree{"footer", std::forward<Args>(args)...} {}
@@ -54,6 +59,21 @@ namespace cxml {
     struct section: tree {
         template<typename... Args>
         section(Args&&... args): tree{"section", std::forward<Args>(args)...} {}
+    };
+
+    struct blockquote: tree {
+        template<typename... Args>
+        blockquote(Args&&... args): tree{"blockquote", std::forward<Args>(args)...} {}
+    };
+
+    struct form: tree {
+        template<typename... Args>
+        form(Args&&... args): tree{"form", std::forward<Args>(args)...} {}
+    };
+
+    struct fieldset: tree {
+        template<typename... Args>
+        fieldset(Args&&... args): tree{"fieldset", std::forward<Args>(args)...} {}
     };
 
     struct ol: tree {
@@ -91,12 +111,111 @@ namespace cxml {
         tr(Args&&... args): tree{"tr", std::forward<Args>(args)...} {}
     };
 
+    struct figure: tree {
+        template<typename... Args>
+        figure(Args&&... args): tree{"figure", std::forward<Args>(args)...} {}
+    };
+
+    struct figcaption: tree {
+        template<typename... Args>
+        figcaption(Args&&... args): tree{"figcaption", std::forward<Args>(args)...} {}
+    };
+
+    struct select: tree {
+        template<typename... Args>
+        select(Args&&... args): tree{"select", std::forward<Args>(args)...} {}
+    };
+
+    struct legend: tree {
+        template<typename... Args>
+        legend(Args&&... args): tree{"legend", std::forward<Args>(args)...} {}
+    };
+
+    struct details: tree {
+        template<typename... Args>
+        details(Args&&... args): tree{"details", std::forward<Args>(args)...} {}
+    };
+
+    struct progress: tree {
+        template<typename... Args>
+        progress(Args&&... args): tree{"progress", std::forward<Args>(args)...} {}
+    };
+
+    struct dialog: tree {
+        template<typename... Args>
+        dialog(Args&&... args): tree{"dialog", std::forward<Args>(args)...} {}
+    };
+
+    struct span: content {
+        span(std::string_view text): content{"span", text} {}
+    };
+
     struct title: content {
         title(std::string_view text): content{"title", text} {}
     };
 
+    struct a: content {
+        a(std::string_view text): content{"a", text} {}
+    };
+
     struct h1: content {
         h1(std::string_view text): content{"h1", text} {}
+    };
+
+    struct h2: content {
+        h2(std::string_view text): content{"h2", text} {}
+    };
+
+    struct h3: content {
+        h3(std::string_view text): content{"h3", text} {}
+    };
+
+    struct h4: content {
+        h4(std::string_view text): content{"h4", text} {}
+    };
+
+    struct h5: content {
+        h5(std::string_view text): content{"h5", text} {}
+    };
+
+    struct h6: content {
+        h6(std::string_view text): content{"h6", text} {}
+    };
+
+    struct strong: content {
+        strong(std::string_view text): content{"strong", text} {}
+    };
+
+    struct em: content {
+        em(std::string_view text): content{"em", text} {}
+    };
+
+    struct u: content {
+        u(std::string_view text): content{"u", text} {}
+    };
+
+    struct ins: content {
+        ins(std::string_view text): content{"ins", text} {}
+    };
+
+    struct del: content {
+        del(std::string_view text): content{"del", text} {}
+    };
+
+    struct sub: content {
+        sub(std::string_view text): content{"sub", text} {}
+    };
+
+    struct sup: content {
+        sup(std::string_view text): content{"sup", text} {}
+    };
+
+    struct small: content {
+        small(std::string_view text): content{"small", text} {}
+    };
+
+    struct s: content {
+        s(std::string_view text): content{"s", text} {}
     };
 
     struct p: content {
@@ -109,6 +228,34 @@ namespace cxml {
 
     struct code: content {
         code(std::string_view text): content{"code", text} {}
+    };
+
+    struct cite: content {
+        cite(std::string_view text): content{"cite", text} {}
+    };
+
+    struct abbr: content {
+        abbr(std::string_view text): content{"abbr", text} {}
+    };
+
+    struct kbd: content {
+        kbd(std::string_view text): content{"kbd", text} {}
+    };
+
+    struct mark: content {
+        mark(std::string_view text): content{"mark", text} {}
+    };
+
+    struct summary: content {
+        summary(std::string_view text): content{"summary", text} {}
+    };
+
+    struct label: content {
+        label(std::string_view text): content{"label", text} {}
+    };
+
+    struct option: content {
+        option(std::string_view text): content{"option", text} {}
     };
 
     struct button: content {
@@ -137,6 +284,10 @@ namespace cxml {
 
     struct img: self_closing {
         img(): self_closing{"img"} {}
+    };
+
+    struct input: self_closing {
+        input(): self_closing{"input"} {}
     };
 
 }
--- example/server.cpp
+++ example/server.cpp
@@ -1,3 +1,7 @@
+// https://github.com/picocss/examples/blob/master/v2-html-classless/index.html
+
+#include <print>
+#include <utility>
 #include "cpp-httplib/httplib.h"
 #include "../cxml_html.hpp"
 
@@ -5,7 +9,9 @@ auto main() -> int {
     httplib::Server server;
 
     server.Get("/", [](const httplib::Request&, httplib::Response& res) {
-        cxml::meta theme;
+        using namespace cxml;
+
+        meta theme;
         theme.attributes["name"] = "colorscheme";
         theme.attributes["content"] = "light dark";
 
@@ -13,15 +19,57 @@ auto main() -> int {
         style.attributes["rel"] = "stylesheet";
         style.attributes["href"] = "https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.classless.min.css";
 
-        cxml::html doc {
-            cxml::head {
+        a home_button { "Home" };
+        home_button.attributes["href"] = "/";
+
+        a articles_button { "Articles" };
+        articles_button.attributes["href"] = "/articles";
+
+        a contact_button { "Contact" };
+        contact_button.attributes["href"] = "/contact";
+
+        a pico { "Pico" };
+        pico.attributes["href"] = "https://picocss.com";
+
+        html doc {
+            head {
                 std::move(theme),
                 std::move(style),
-                cxml::title { "CXML example" }
+                title { "CXML example" }
             },
-            cxml::body {
+            body {
+                header {
+                    hgroup {
+                        h1 { "CXML example" },
+                        p { "Github: gab462/cxml" }
+                    },
+                    nav {
+                        ul {
+                            li { home_button.render() },
+                            li { articles_button.render() },
+                            li { contact_button.render() }
+                        }
+                    }
+                },
                 cxml::main {
-                    cxml::h1 { "Hello, World!" }
+                    article {
+                        h2 { "Hello, World!" },
+                        p { "Nullam dui arcu, malesuada et sodales eu, efficitur vitae dolor. Sed ultricies dolor non ante vulputate hendrerit. Vivamus sit amet suscipit sapien. Nulla iaculis eros a elit pharetra egestas. Nunc placerat facilisis cursus. Sed vestibulum metus eget dolor pharetra rutrum." },
+                        blockquote {
+                            span { "\"Maecenas vehicula metus tellus, vitae congue turpis hendrerit non. Nam at dui sit amet ipsum cursus ornare.\"" },
+                            footer {
+                                cite { "- Phasellus eget lacinia" }
+                            }
+                        },
+                        footer {
+                            small { "Duis nec elit placerat, suscipit nibh quis, finibus neque." }
+                        }
+                    }
+                },
+                footer {
+                    small {
+                        std::string{"Built with "} + pico.render()
+                    }
                 }
             }
         };
@@ -29,7 +77,9 @@ auto main() -> int {
         res.set_content(std::string{"<!DOCTYPE html>"} + doc.render(), "text/html");
     });
 
-    server.listen("0.0.0.0", 8080);
+    constexpr int port = 8080;
+    std::println("Listening on port {}...", port);
+    server.listen("0.0.0.0", port);
 
     return 0;
 }
